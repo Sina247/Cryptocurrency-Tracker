fe5d5bcd338bf0f3df52c1031046a5d81033d6b2
Sina Ahmadi <sina247re@gmail.com>
2025-12-27 10:52:58 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Home } from "./pages/Home";
+import { CoinDetail } from "./pages/CoinDetail";
+import { BrowserRouter, Routes, Route } from "react-router";
+
+function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/coin/:id" element={<CoinDetail />} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default App;
